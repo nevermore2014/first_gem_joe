d13d8565ae0114051c70a170855cc07264052f7d
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-12 12:04:09 -0500

    more test

--- spec/first_gem_joe_spec.rb
+++ spec/first_gem_joe_spec.rb
@@ -10,4 +10,17 @@ describe String, "#word_count" do
     the_word_count = a_string.word_count
     expect(the_word_count).to eq 1
   end
+
+  it "should return 1 when the sting is one word long" do
+    expect('apple'.word_count).to eq 1
+  end
+
+  it "should return 2 when the string is two word long" do
+    expect('apple pie'.word_count).to eq 2
+  end
+
+  # it "should return apple when wht string only have one apple " do
+  #   expect('apple pie'.u)
+  # end
+
 end
